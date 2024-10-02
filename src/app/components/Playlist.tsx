83cef6ff0454489ee2cc13700ad0124187114180
HydroponKing <155968943+HydroponKing@users.noreply.github.com>
2024-10-02 21:26:46 +0500

    список песен из api

--- src/app/components/Playlist.tsx
+++ src/app/components/Playlist.tsx
@@ -1,47 +1,24 @@
+import React from "react";
+import { PlaylistData } from "./PlaylistData"; // Импортируем компонент с данными
+
 export default function Playlist() {
     return (
       <div className="centerblock__content playlist-content">
-        <div className="content__title playlist-title">
-          <div className="playlist-title__col col01">Трек</div>
-          <div className="playlist-title__col col02">Исполнитель</div>
-          <div className="playlist-title__col col03">Альбом</div>
-          <div className="playlist-title__col col04">
-            <svg className="playlist-title__svg">
-              <use xlinkHref="/img/icon/sprite.svg#icon-watch"></use>
-            </svg>
-          </div>
-        </div>
-        <div className="content__playlist playlist">
-          {/* Здесь добавьте список треков */}
-          <div className="playlist__item">
-            <div className="playlist__track track">
-              <div className="track__title">
-                <div className="track__title-image">
-                  <svg className="track__title-svg">
-                    <use xlinkHref="/img/icon/sprite.svg#icon-note"></use>
-                  </svg>
-                </div>
-                <div className="track__title-text">
-                  <a className="track__title-link" href="http://">Guilt</a>
-                </div>
-              </div>
-              <div className="track__author">
-                <a className="track__author-link" href="http://">Nero</a>
-              </div>
-              <div className="track__album">
-                <a className="track__album-link" href="http://">Welcome Reality</a>
-              </div>
-              <div className="track__time">
-                <svg className="track__time-svg">
-                  <use xlinkHref="/img/icon/sprite.svg#icon-like"></use>
-                </svg>
-                <span className="track__time-text">4:44</span>
-              </div>
-            </div>
-          </div>
-          {/* Другие треки */}
+      <div className="content__title playlist-title">
+        <div className="playlist-title__col col01">Трек</div>
+        <div className="playlist-title__col col02">Исполнитель</div>
+        <div className="playlist-title__col col03">Альбом</div>
+        <div className="playlist-title__col col04">
+          <svg className="playlist-title__svg">
+            <use xlinkHref="/img/icon/sprite.svg#icon-watch"></use>
+          </svg>
         </div>
       </div>
+      
+      {/* Вставляем компонент с логикой API */}
+      <PlaylistData />
+      
+    </div>
     );
   }
   
---
+++ src/app/components/PlaylistData.tsx
@@ -0,0 +1,94 @@
+ "use client"
+
+import React, { useState, useEffect } from "react";
+import axios from "axios";
+
+//интерфейс для трека
+interface Track {
+    _id: number;
+    name: string;
+    author: string;
+    release_date: string;
+    genre: string[];
+    duration_in_seconds: number;
+    album: string;
+    logo: {
+      type: string;
+      data: any[]; // Тут скорее всего массив байтов, для упрощения оставим как any[], позже уточним структуру. Понимаю что лучше рабоать без any но тут я думаю это не критично ^^
+    };
+    track_file: string;
+    staredUser: number[];
+  }
+
+export const PlaylistData = () => {
+  const [tracks, setTrack] = useState<Track[]>([]);
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState<Error | null>(null);
+
+  useEffect(() => {
+    const fetchTracks = async () => {
+      try {
+        const response = await axios.get(
+          "https://webdev-music-003b5b991590.herokuapp.com/catalog/track/all/"
+        );
+        setTrack(response.data.data);
+        setLoading(false);
+      } catch (err: unknown) {
+        setError(handleError(err));
+        setLoading(false);
+      }
+    };
+
+    fetchTracks();
+  }, []);
+
+  if (loading) {
+    return <div>Loading...</div> //колхоз пока, далее заменим на скелетон ^^
+  }
+  if (error) {
+    return <div>Error: {error.message} </div>
+  }
+
+  return (
+    <div className="content__playlist playlist">
+      {tracks.map((track) => (
+        <div key={track._id} className="playlist__item">
+          <div className="playlist__track track">
+            <div className="track__title">
+              <div className="track__title-image">
+                <svg className="track__title-svg">
+                  <use xlinkHref="/img/icon/sprite.svg#icon-note"></use>
+                </svg>
+              </div>
+              <div className="track__title-text">
+                <a className="track__title-link" href={track.track_file}>{track.name}</a>
+              </div>
+            </div>
+            <div className="track__author">
+              <a className="track__author-link" href="#">{track.author}</a>
+            </div>
+            <div className="track__album">
+              <a className="track__album-link" href="#">{track.album}</a>
+            </div>
+            <div className="track__time">
+              <span className="track__time-text">{formatDuration(track.duration_in_seconds)}</span>
+            </div>
+          </div>
+        </div>
+      ))}
+    </div>
+  );
+};
+
+const formatDuration = (seconds: number) => {
+    const minutes = Math.floor(seconds / 60);
+    const remainingSeconds = seconds % 60;
+    return `${minutes}:${remainingSeconds < 10 ? "0" : ""}${remainingSeconds}`;
+  };
+
+const handleError = (err: unknown): Error => {
+    if (err instanceof Error) {
+      return err;
+    }
+    return new Error('Произошла неизвестная ошибка');
+  };
